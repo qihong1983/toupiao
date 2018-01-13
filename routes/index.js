860f134bde9f6b1fd26fc3df6848abec576058e8
bjqihong <bjqihong@corp.netease.com>
2018-01-13 11:41:44 +0800

    修改

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host: '39.106.140.80',
+	user: 'root',
+	password: 'Qihong38752673',
+	database: 'toupiao'
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', {
+		title: 'Express'
+	});
 });
 
 module.exports = router;
