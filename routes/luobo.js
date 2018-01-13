b06133b283f89511c899a675075899e3aa0300dc
bjqihong <bjqihong@corp.netease.com>
2018-01-13 13:58:12 +0800

    asdfasdf

---
+++ routes/luobo.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host: '39.106.140.80',
+	user: 'root',
+	password: 'Qihong38752673',
+	database: 'toupiao'
+});
+
+//执行创建连接 
+connection.connect();
+
+//SQL语句
+var sql = 'SELECT icon FROM lunbo';
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+
+	connection.query(sql, function(err, result) {
+
+		var arr = [];
+		if (err) {
+			// console.log('[SELECT ERROR] - ', err.message);
+
+			res.json({
+				status: false,
+				msg: err.message
+			})
+			return;
+		}
+
+
+		result.forEach(function(v, k) {
+
+			arr.push({
+				images: v.icon
+			})
+		})
+
+		//把搜索值输出
+		res.json({
+			status: true,
+			msg: "成功",
+			data: arr
+		});
+	});
+
+});
+
+module.exports = router;
